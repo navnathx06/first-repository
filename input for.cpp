13652b3a47c77922d05c6082dc666d41ff4c1df4
Navnath <navnyajade@gmail.com>
2026-04-20 18:29:03 +0530

    Take input from user

---
+++ input for.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 20, 30, 40};
+    int n = 4;
+
+    for(int i = 0; i < n; i++) {
+        cout << "Index " << i << " = " << arr[i] << endl;
+    }
+
+    return 0;
+}
