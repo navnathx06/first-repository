163605dbe532b4884c42780700fa41c5a67849fe
Navnath <navnyajade@gmail.com>
2026-04-26 10:21:12 +0530

    Data types example

---
+++ datatypes.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //int datatype
+
+    int apples = 5;
+    cout<<apples<<endl;
+    cout<<"size of int : "<<sizeof(apples)<<endl;
+
+    //char datatype
+
+    char sign = '&';
+    cout<<sign<<endl;
+    cout<<"size of int : "<<sizeof(sign)<<endl;
+
+   // bool datatype
+
+    bool flag = true;
+    cout<<flag<<endl;
+    cout<<"size of int : "<<sizeof(flag)<<endl;
+
+    //float datatype
+
+    float score = 5.43;
+    cout<<score<<endl;
+    cout<<"size of int : "<<sizeof(score)<<endl;
+
+    return 0;
+}
